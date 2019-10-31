be14bcb3251987a0b72f3bc17b5c0a43b4f288db
Fabio Anderegg <fabio.anderegg@zebbra.ch>
2019-10-31 17:07:52 +0100

    add editor with twig syntax highlight and autocomplete

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <MonacoEditor class="editor" v-model="code" language="json" theme="vs-dark" :options="options" />
+    <MonacoEditor
+      class="editor"
+      v-model="code"
+      language="jinja2"
+      theme="vs-dark"
+      :options="options"
+      @editorDidMount="editorDidMount"
+      ref="editor"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import MonacoEditor from "vue-monaco";
+
+import { conf, language } from "./jinja2";
 
 @Component({
   components: {
@@ -20,6 +30,27 @@ export default class App extends Vue {
       enabled: false
     }
   };
+
+  editorDidMount(editor: any) {
+    const monaco = (this.$refs.editor as any).monaco;
+
+    monaco.languages.register({ id: "jinja2" });
+    monaco.languages.setLanguageConfiguration("jinja2", conf);
+    monaco.languages.setMonarchTokensProvider("jinja2", language);
+
+    monaco.languages.registerCompletionItemProvider("jinja2", {
+      provideCompletionItems: () => {
+        const suggestions = [
+          {
+            label: "simpleText",
+            kind: monaco.languages.CompletionItemKind.Text,
+            insertText: "simpleText"
+          }
+        ];
+        return { suggestions };
+      }
+    });
+  }
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new MonacoEditorPlugin({
-        languages: [],
         features: []
       })
     ]
